450217ab74632250c0dd5829767fd29cd7c698a5
krispydev <chrispate07@gmail.com>
2023-01-25 11:24:54 +0530

    first edits for eip712

--- test/primaryTask.ts
+++ test/primaryTask.ts
@@ -1,52 +1,52 @@
-import { expect } from "chai";
-import { ethers } from "hardhat";
-// import { ethers } from "ethers";
-import "dotenv/config"
-// import "@nomiclabs/hardhat-ethers"; This plugins adds an ethers object to the Hardhat Runtime Environment. We already have one
-// import "@nomicfoundation/hardhat-network-helpers";
+// import { expect } from "chai";
+// import { ethers } from "hardhat";
+// // import { ethers } from "ethers";
+// import "dotenv/config"
+// // import "@nomiclabs/hardhat-ethers"; This plugins adds an ethers object to the Hardhat Runtime Environment. We already have one
+// // import "@nomicfoundation/hardhat-network-helpers";
 
-describe("Transaction using private keys on Venidium Testnet", function () {
-  it("Transaction whose private key I have access to is recorded", async function () {
+// describe("Transaction using private keys on Venidium Testnet", function () {
+//   it("Transaction whose private key I have access to is recorded", async function () {
     
-    // Gets first two accounts set in hardhat.config.ts
-    const [user1, user2] = await ethers.getSigners();
+//     // Gets first two accounts set in hardhat.config.ts
+//     const [user1, user2] = await ethers.getSigners();
 
-    // Account address of Account 2
-    const addressTo = user2.address; 
+//     // Account address of Account 2
+//     const addressTo = user2.address; 
 
-    // 4. Create send function
-    // Sending a transaction from Account 1 to Account 2
-    const send = async () => {
-      console.log(`Attempting to send transaction from ${user1.address} to ${addressTo}`);
+//     // 4. Create send function
+//     // Sending a transaction from Account 1 to Account 2
+//     const send = async () => {
+//       console.log(`Attempting to send transaction from ${user1.address} to ${addressTo}`);
 
-      // 5. Create tx object
-      const tx = {
-        to: addressTo,
-        value: ethers.utils.parseEther('1'),
-      };
-      // // console.log(tx);
+//       // 5. Create tx object
+//       const tx = {
+//         to: addressTo,
+//         value: ethers.utils.parseEther('1'),
+//       };
+//       // // console.log(tx);
 
-      // 6. Sign and send tx - wait for receipt
-      const createReceipt = await user1.sendTransaction(tx);      
-      await createReceipt.wait();
-      // console.log(createReceipt);
-      console.log(`\nTransaction successful with hash: ${createReceipt.hash} \n`);
-    };
+//       // 6. Sign and send tx - wait for receipt
+//       const createReceipt = await user1.sendTransaction(tx);      
+//       await createReceipt.wait();
+//       // console.log(createReceipt);
+//       console.log(`\nTransaction successful with hash: ${createReceipt.hash} \n`);
+//     };
 
-    console.log("\nAccount Balance of sending account BEFORE calling send() :-");
-    console.log("Address: ", user1.address);
-    console.log("Balance: ", await (await ethers.provider.getBalance(user1.address)).toString());
+//     console.log("\nAccount Balance of sending account BEFORE calling send() :-");
+//     console.log("Address: ", user1.address);
+//     console.log("Balance: ", await (await ethers.provider.getBalance(user1.address)).toString());
 
-    // Call the send function
-    console.log("\n");
-    await send();
+//     // Call the send function
+//     console.log("\n");
+//     await send();
     
-    console.log("\nAccount Balances of sending account AFTER calling send() :-");
-    console.log("Balance: ", await (await ethers.provider.getBalance(user1.address)).toString());
+//     console.log("\nAccount Balances of sending account AFTER calling send() :-");
+//     console.log("Balance: ", await (await ethers.provider.getBalance(user1.address)).toString());
 
-    // Checking the Test Case
-    await expect(await ethers.provider.getBalance(user2.address)).to.equal(
-      ethers.utils.parseEther('10001')
-    )
-  });
-});
+//     // Checking the Test Case
+//     await expect(await ethers.provider.getBalance(user2.address)).to.equal(
+//       ethers.utils.parseEther('10001')
+//     )
+//   });
+// });
--- test/secondaryTask.ts
+++ test/secondaryTask.ts
@@ -1,47 +1,47 @@
-import { expect } from "chai";
-// import { ethers } from "ethers";
-import { ethers } from "hardhat";
-import "dotenv/config"
-// import "@nomiclabs/hardhat-ethers"; This plugins adds an ethers object to the Hardhat Runtime Environment. We already have one
-// import "@nomicfoundation/hardhat-network-helpers";
+// import { expect } from "chai";
+// // import { ethers } from "ethers";
+// import { ethers } from "hardhat";
+// import "dotenv/config"
+// // import "@nomiclabs/hardhat-ethers"; This plugins adds an ethers object to the Hardhat Runtime Environment. We already have one
+// // import "@nomicfoundation/hardhat-network-helpers";
 
-describe("Transaction WITHOUT using private keys on Venidium Testnet", function () {
-  it("Transaction whose private key I DON'T have access to is recorded", async function () {
+// describe("Transaction WITHOUT using private keys on Venidium Testnet", function () {
+//   it("Transaction whose private key I DON'T have access to is recorded", async function () {
 
-    this.signers = await ethers.getSigners()
-    this.user1 = this.signers[0];
-    this.user2 = this.signers[1];
-    this.user3 = this.signers[2];
-    this.user4 = this.signers[3];
-    const addressTo = this.user3.address;
+//     this.signers = await ethers.getSigners()
+//     this.user1 = this.signers[0];
+//     this.user2 = this.signers[1];
+//     this.user3 = this.signers[2];
+//     this.user4 = this.signers[3];
+//     const addressTo = this.user3.address;
 
-    const helpers = require("@nomicfoundation/hardhat-network-helpers");
+//     const helpers = require("@nomicfoundation/hardhat-network-helpers");
 
-    // Address that will send the ether
-    const impersonatedAccount = this.user4.address
+//     // Address that will send the ether
+//     const impersonatedAccount = this.user4.address
 
-    // impersonating an account
-    await helpers.impersonateAccount(impersonatedAccount);
-    const impersonatedSigner = await ethers.provider.getSigner(impersonatedAccount);
-    // console.log("\nimpersonatedSigner: \n", impersonatedSigner);
+//     // impersonating an account
+//     await helpers.impersonateAccount(impersonatedAccount);
+//     const impersonatedSigner = await ethers.provider.getSigner(impersonatedAccount);
+//     // console.log("\nimpersonatedSigner: \n", impersonatedSigner);
 
-    console.log("\nBalance of Impersonated Account BEFORE transaction = ", await (await ethers.provider.getBalance(impersonatedAccount)).toString());
-    console.log("\nBalance of account to be credited BEFORE transaction = ", await (await ethers.provider.getBalance(addressTo)).toString());
+//     console.log("\nBalance of Impersonated Account BEFORE transaction = ", await (await ethers.provider.getBalance(impersonatedAccount)).toString());
+//     console.log("\nBalance of account to be credited BEFORE transaction = ", await (await ethers.provider.getBalance(addressTo)).toString());
 
-    // Sending the transaction
-    const tx = {
-      to: addressTo,
-      value: ethers.utils.parseEther('1'),
-    };
-    await impersonatedSigner.sendTransaction(tx);
+//     // Sending the transaction
+//     const tx = {
+//       to: addressTo,
+//       value: ethers.utils.parseEther('1'),
+//     };
+//     await impersonatedSigner.sendTransaction(tx);
 
-    console.log("\nBalance of Impersonated Account AFTER transaction = ", await (await ethers.provider.getBalance(impersonatedAccount)).toString());
-    console.log("\nBalance of account to be credited AFTER transaction = ", await (await ethers.provider.getBalance(addressTo)).toString());
+//     console.log("\nBalance of Impersonated Account AFTER transaction = ", await (await ethers.provider.getBalance(impersonatedAccount)).toString());
+//     console.log("\nBalance of account to be credited AFTER transaction = ", await (await ethers.provider.getBalance(addressTo)).toString());
     
 
-    // Checking the Test Case
-    await expect(await ethers.provider.getBalance(this.user3.address)).to.equal(
-      ethers.utils.parseEther('10001')
-    )
-    });
-});
+//     // Checking the Test Case
+//     await expect(await ethers.provider.getBalance(this.user3.address)).to.equal(
+//       ethers.utils.parseEther('10001')
+//     )
+//     });
+// });
